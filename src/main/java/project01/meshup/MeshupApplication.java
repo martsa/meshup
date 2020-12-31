1a5ce3ba464deccbadcf30d82a27de0e2cbe1a22
lobsang <lobsang@lobsangs-MacBook-Pro.local>
2020-12-31 10:19:04 +0100

    Initial commit

---
+++ src/main/java/project01/meshup/MeshupApplication.java
@@ -0,0 +1,13 @@
+package project01.meshup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeshupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeshupApplication.class, args);
+    }
+
+}
---
+++ src/test/java/project01/meshup/MeshupApplicationTests.java
@@ -0,0 +1,13 @@
+package project01.meshup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeshupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
